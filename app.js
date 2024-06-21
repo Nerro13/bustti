4843780fde57a51cc422b07f20ba6426d1468156
Nerro13 <nduguiskarioti@gmail.com>
2024-06-21 17:42:34 +0300

    D2 Home UI

--- app.js
+++ app.js
@@ -6,13 +6,28 @@
 app.use(express.static("public"))
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
+
+
 app.set("view engine", "ejs")
 
 app.get("/", (req,res)=>{
     res.render('home')
 })
-
-
+app.get("/admissions", (req,res)=>{
+    res.render('admissions')
+})
+app.get("/academics",(req,res)=>{
+    res.render("academics")
+})
+app.get("/departments",(req,res)=>{
+    res.render("departments")
+})
+app.get("/services",(req,res)=>{
+    res.render("services")
+})
+app.get("/sports",(req,res)=>{
+    res.render("sports")
+})
 
 app.listen(PORT, ()=>{
     console.log("SERVER IS LIVE")
